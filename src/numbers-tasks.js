c91a39ddd5c43536cf9c68f29db6406b2e286976
VonavstaliN <vonavistalin2009@mail.ru>
2023-11-21 14:11:56 +0100

    feat: do 11 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -197,8 +197,8 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  return n;
+function isPrime(/* n */) {
+  throw new Error('Not implemented');
 }
 
 /**
